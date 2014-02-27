c870411ac841eb7834b8a6e75315783cadfc82ac
Emerson R Orci <emersonorci@hotmail.com>
2014-02-27 10:06:49 -0300

    Working. Mapping was done using hibernate.cfg xml file

--- src/com/livro/capitulo3/crudxml/Contato.java
+++ src/com/livro/capitulo3/crudxml/Contato.java
@@ -9,43 +9,53 @@ public class Contato {
 	private Date dataCadastro;
 	private String observacao;
 	private String telefone;
+
 	public int getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public Date getDataCadastro() {
 		return dataCadastro;
 	}
+
 	public void setDataCadastro(Date dataCadastro) {
 		this.dataCadastro = dataCadastro;
 	}
+
 	public String getObservacao() {
 		return observacao;
 	}
+
 	public void setObservacao(String observacao) {
 		this.observacao = observacao;
 	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-	
-	
-	
+
 }
--- src/com/livro/capitulo3/crudxml/ContatoCrudXML.java
+++ src/com/livro/capitulo3/crudxml/ContatoCrudXML.java
@@ -10,132 +10,148 @@ import org.hibernate.Transaction;
 
 import com.livro.capitulo3.conexao.HibernateUtil;
 
-
 public class ContatoCrudXML {
-	
-	Session sessao = null;
-	
-	public ContatoCrudXML() {
-		sessao = HibernateUtil.getSessionFactory().openSession();
-	}
 
 	public void salvar(Contato contato) {
+		Session sessao = null;
 		Transaction transacao = null;
 		try {
+			sessao = HibernateUtil.getSessionFactory().openSession();
 			transacao = sessao.beginTransaction();
 			sessao.save(contato);
 			transacao.commit();
 		} catch (HibernateException e) {
-			System.out.println("Não foi possível inserir o contato.\nErro: " + e.getMessage());
+			System.out.println("Não foi possível inserir o contato.\nErro: "
+					+ e.getMessage());
 		} finally {
-			try { 
-				sessao.close();
-			} catch (Throwable e) {
-				System.out.println("Falha ao fechar operação de inserção.\nErro: " + e.getMessage());
-			}
-		}
-	}
-	
-	public void atualizar(Contato contato) {
-		Transaction transacao = null;
-		try {
-			transacao = sessao.beginTransaction();
-			sessao.update(contato);
-			transacao.commit();
-		} catch (HibernateException e) {
-			System.out.println("Não foi possível alterar o contato.\nErro: " + e.getMessage());
-		}
-		finally {
 			try {
 				sessao.close();
 			} catch (Throwable e) {
-				System.out.println("Falha ao fechar operação de alteração.\nErro: " + e.getMessage());
+				System.out
+						.println("Falha ao fechar operação de inserção.\nErro: "
+								+ e.getMessage());
 			}
 		}
 	}
-	
-	public void excluir(Contato contato) {
+
+	public void atualizar(Contato contato) {
+		Session sessao = null;
 		Transaction transacao = null;
 		try {
+			sessao = HibernateUtil.getSessionFactory().openSession();
+			sessao.update(contato);
+			transacao.commit();
+		} catch (HibernateException e) {
+			System.out.println("Não foi possível alterar o contato.\nErro: "
+					+ e.getMessage());
+		} finally {
+			try {
+				sessao.close();
+			} catch (Throwable e) {
+				System.out
+						.println("Falha ao fechar operação de alteração.\nErro: "
+								+ e.getMessage());
+			}
+		}
+	}
+
+	public void excluir(Contato contato) {
+		Session sessao = null;
+		Transaction transacao = null;
+		try {
+			sessao = HibernateUtil.getSessionFactory().openSession();
 			transacao = sessao.beginTransaction();
 			sessao.delete(contato);
 			transacao.commit();
 		} catch (HibernateException e) {
-			System.out.println("Não foi possível excluir o contato.\nErro: " + e.getMessage());
-		}
-		finally {
+			System.out.println("Não foi possível excluir o contato.\nErro: "
+					+ e.getMessage());
+		} finally {
 			try {
 				sessao.close();
 			} catch (Throwable e) {
-				System.out.println("Falha ao fechar operação de alteração.\nErro: " + e.getMessage());
+				System.out
+						.println("Falha ao fechar operação de alteração.\nErro: "
+								+ e.getMessage());
 			}
 		}
 	}
-	
+
 	public List<Contato> listar() {
+		Session sessao = null;
 		Transaction transacao = null;
 		Query consulta = null;
-		List<Contato> resultado  = null;
+		List<Contato> resultado = null;
 		try {
-			transacao= sessao.beginTransaction();
+			sessao = HibernateUtil.getSessionFactory().openSession();
+			transacao = sessao.beginTransaction();
 			consulta = sessao.createQuery("from Contato");
 			resultado = consulta.list();
 			transacao.commit();
 			return resultado;
 		} catch (HibernateException e) {
-			System.out.println("Não foi possível excluir o contato.\nErro: " + e.getMessage());
+			System.out.println("Não foi possível excluir o contato.\nErro: "
+					+ e.getMessage());
 			throw new HibernateException(e);
-		}
-		finally {
+		} finally {
 			try {
 				sessao.close();
 			} catch (Throwable e) {
-				System.out.println("Falha ao fechar operação de alteração.\nErro: " + e.getMessage());
+				System.out
+						.println("Falha ao fechar operação de alteração.\nErro: "
+								+ e.getMessage());
 			}
 		}
 	}
 
 	public Contato buscaContato(long valor) {
+		Session sessao = null;
 		Contato contato = null;
 		Transaction transacao = null;
 		Query consulta = null;
 		try {
-			transacao= sessao.beginTransaction();
-			consulta = sessao.createQuery("from Contato where codigo = :parametro");
+			sessao = HibernateUtil.getSessionFactory().openSession();
+			transacao = sessao.beginTransaction();
+			consulta = sessao
+					.createQuery("from Contato where codigo = :parametro");
 			consulta.setLong("parametro", valor);
 			contato = (Contato) consulta.uniqueResult();
 			transacao.commit();
 			return contato;
 		} catch (HibernateException e) {
-			System.out.println("Não foi possível excluir o contato.\nErro: " + e.getMessage());
+			System.out.println("Não foi possível excluir o contato.\nErro: "
+					+ e.getMessage());
 			throw new HibernateException(e);
-		}
-		finally {
+		} finally {
 			try {
 				sessao.close();
 			} catch (Throwable e) {
-				System.out.println("Falha ao fechar operação de alteração.\nErro: " + e.getMessage());
+				System.out
+						.println("Falha ao fechar operação de alteração.\nErro: "
+								+ e.getMessage());
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		ContatoCrudXML contatoCrudXML = new ContatoCrudXML();
-		String[] nomes = {"Emerson","Rodrigo","Orci"};
-		String[] fones = {"(47)2222-2222","(47)23232-5454","(19)4542-0909"};
-		String[] emails ={"emerson@teste.com","rodrigo@teste.com","orci@teste.com"};
-		String[] obs = {"Novo cliente","Cliente bom pagador","Ligar urgente"};
+		String[] nomes = { "Emerson", "Rodrigo", "Orci" };
+		String[] fones = { "(47)2222-2222", "(47)23232-5454", "(19)4542-0909" };
+		String[] emails = { "emerson@teste.com", "rodrigo@teste.com",
+				"orci@teste.com" };
+		String[] obs = { "Novo cliente", "Cliente bom pagador", "Ligar urgente" };
 		Contato contato = null;
-		for(int i=0;i<=nomes.length; i++) {
+		for (int i = 0; i < nomes.length; i++) {
 			contato = new Contato();
 			contato.setNome(nomes[i]);
 			contato.setDataCadastro(new Date(System.currentTimeMillis()));
+			contato.setTelefone(fones[i]);
 			contato.setEmail(emails[i]);
 			contato.setObservacao(obs[i]);
 			contatoCrudXML.salvar(contato);
 		}
-		System.out.println("Total de registros na tabela de Contatos " + contatoCrudXML.listar().size());
+		System.out.println("Total de registros na tabela de Contatos "
+				+ contatoCrudXML.listar().size());
 	}
 
-	
 }
